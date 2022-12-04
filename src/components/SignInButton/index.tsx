d09ce03746c12104c664ee0880dd4194980a1a2f
Carlos Augusto <carlosmiranda19122@gmail.com>
2022-12-04 16:05:22 -0300

    created component and stylized button of SIGNIN with github

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,24 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#04D361" />
+        SMeu egg
+        <FiX color="#737380" />
+      </button>
+    </>
+  ) : (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#EBA417" />
+        Sign in with Github
+      </button>
+    </>
+  );
+}
