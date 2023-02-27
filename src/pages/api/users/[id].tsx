4bcabbc2255770f32e6098580d462b97d2c4412d
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-02-27 20:40:28 -0300

    example of requests in the frontend(request users)

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { NextApiRequest, NextApiResponse } from "next";
+
+/** Because Next app's have a extra server node to create the HTML file for return
+ * to client, this same server node, can to make requests, responses and store datas too
+ */
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+  console.log(request.query);
+
+  const users = [
+    { id: 1, name: "Diego" },
+    { id: 3, name: "Carlos" },
+    { id: 4, name: "Julia" },
+  ];
+
+  return response.json(users);
+};
