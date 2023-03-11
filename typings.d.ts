b7b27537797f050ed10e499ad1b2658878a7860b
Carlos Augusto <carlosmiranda19122@gmail.com>
2023-03-11 16:49:42 -0300

    created a webhook to stripe

--- typings.d.ts
+++ typings.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
     PORT: string;
     STRIPE_API_KEY: string;
     NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
+    STRIPE_WEBHOOK_SECRET: string;
     STRIPE_SUCCESS_URL: "http://localhost:3000/posts";
     STRIPE_CANCEL_URL: "http://localhost:3000/";
     GITHUB_CLIENT_ID: string;
